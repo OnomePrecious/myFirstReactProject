83f862ded53597e13661c3ac285797b2c3d39a05
Precious <preciousonome002@gmail.com>
2024-06-03 13:48:34 +0100

    firstReactProject

---
+++ src/Pages/Home/Marketing/data.js
@@ -0,0 +1,20 @@
+import woman from "../../../asset/woman.png"
+import laptop from "../../../asset/laptop.png"
+import cofpic from "../../../asset/cofpic.png"
+
+export const market =[
+    {
+        image: woman,
+        text: "Creating Streamlined Safeguarding Processes with OneRen;"
+    },
+
+    {
+        image: laptop,
+        text: "What are your safeguarding responsibilities and how can you manage them?"
+    },
+
+    {
+        image: cofpic,
+        text: "Revamping the Membership Model with Triathlon Australia."
+    }
+]
--- src/Pages/Home/Marketing/index.jsx
+++ src/Pages/Home/Marketing/index.jsx
@@ -1,51 +1,42 @@
 import style from './index.module.css'
-import woman from "../../../asset/woman.png"
-import laptop from "../../../asset/laptop.png"
-import cofpic from "../../../asset/cofpic.png"
+import {market} from "./data";
 const Marketing = () =>{
     return(
         <>
             <div className={style.mainContainer}>
-                <h2>Caring is the new marketing</h2>
+                <h2 style={{textAlign: "center",}}>Caring is the new marketing</h2>
+                <p style={{textAlign: "center"}}>The Nexcent blog is the best place to read about the latest membership insights, trends and more. See who's joining the community,
+                    read about how our community are increasing their membership income and lot's more.</p>
+            </div>
+            <div className={style.imageContainer}>
+                {
+                    market.map((value, index) => (
 
-            <div className={style.textContainer}>
-                <p>The Nexcent blog is the best place to read about the latest membership insights,
-                    <br/>trends and more. See who's joining the community, read about how our community
-                    <br/> are increasing their membership income and lot's more.</p>
+                        <div key={index} style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
+                            <img src={value.image} alt=""/>
+
+                            <p style={{width: "50%", textAlign: "center", fontSize: "25px", marginTop: "-30px", position: "relative", borderRadius: "5px", backgroundColor: "#fff"}}>
+                                {value.text}
+
+
+                            </p>
+                        </div>
+                    ))}
+
+
             </div>
-            <div className={style.firstContainer}>
-                <img src={woman}/>
-                <p> Creating Streamlined <br/> Safeguarding Processes with <br/>OneRen </p>
-                <button className={style.buttonText}>
-                    <p>Readmore -> </p>
-                </button>
-            </div>
-<div className={style.secondContainer}>
-                <img src={laptop}/>
-                <p> What are your Safeguarding <br/> responsibilities and how can <br/>you manage them?</p>
-                <button className={style.buttonText}>
-                    <p>Readmore -> </p>
-                </button>
-</div>
-            <div className={style.thirdContainer}>
-                    <img src={cofpic}/>
-                    <p>Revamping the Membership Model with Triathlon Australia</p>
-                    <button>
-                        <p>Readmore -> </p>
-                    </button>
-            </div>
+
             <div className={style.buttonContainer}>
                 <button
                     style={{
                         color: "white",
                         background: "#4caf4f",
-                    border: "none",
-                    borderRadius: "5px",
-                    padding: "15px 30px"
-                }}>
-                Get a demo ->
-            </button>
-            </div>
+                        border: "none",
+                        borderRadius: "5px",
+                        padding: "15px 30px"
+                    }}>
+                    Get a demo ->
+                </button>
             </div>
         </>
     )
